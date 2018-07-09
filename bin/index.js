6616c8931830d543d16274271cb39a74d15906f0
Roger <luojie.5408@163.com>
2018-07-09 11:20:19 +0800

    0.04

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,27 @@
 #!/usr/bin/env node
  console.log('Hello, world!')
  var shell = require('shelljs');
-
- console.log('first')
- if (!shell.which('git')) {
-   shell.echo('Sorry, this script requires git');
-   shell.exit(1);
- }
- shell.exec('git clone https://github.com/RoggerLuo/react-scaffold.git')
- shell.echo('下载数据')
- shell.exit(1);
- 
-
- // var program = require('commander')
+ var program = require('commander')
 // co = require('co');
 
 // const appInfo = require('./../package.json'),
 //   asyncFunc = require('./../common/asyncfunc.js');
 
-// program.allowUnknownOption();
+program.allowUnknownOption();
 // // program.version(appInfo.version);
 
-// program
-//     .command('init')
-//     .description('初始化当前目录文件')
-//     .action(() => {
-//     })
+program
+    .command('init')
+    .description('初始化当前目录文件')
+    .action(() => {
+        if (!shell.which('git')) {
+          shell.echo('Sorry, this script requires git');
+          shell.exit(1);
+        }
+        shell.echo('下载数据...')
+        shell.exec('git clone https://github.com/RoggerLuo/react-scaffold.git')
+        shell.cp('-R', 'react-scaffold/', './');
+
+        shell.exit(1);
+
+    })
