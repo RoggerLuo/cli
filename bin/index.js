3a34ff08632894f435f98c9be67a414328b01382
Roger <luojie.5408@163.com>
2018-07-09 11:02:07 +0800

    0.0.2

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
-console.log('Hello, world!')
+ // console.log('Hello, world!')
+ var shell = require('shelljs');
 
-// const program = require('commander'),
-//   co = require('co');
+ const program = require('commander')
+// co = require('co');
 
 // const appInfo = require('./../package.json'),
 //   asyncFunc = require('./../common/asyncfunc.js');
 
-// program.allowUnknownOption();
+program.allowUnknownOption();
 // program.version(appInfo.version);
 
-// program
-//   .command('init')
-//   .description('初始化当前目录doc.json文件')
-//   .action(() => co(asyncFunc.initAction));
+program
+    .command('init')
+    .description('初始化当前目录文件')
+    .action(() => {
+        console.log('first')
+        if (!shell.which('git')) {
+          shell.echo('Sorry, this script requires git');
+          shell.exit(1);
+        }
+        shell.exec('git clone https://github.com/RoggerLuo/react-scaffold.git')
+        shell.echo('下载数据')
+        shell.exit(1);
+        
+    })
