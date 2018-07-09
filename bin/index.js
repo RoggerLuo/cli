fbf75cc9760655e9f37abd020d90c50bf9d0a224
Roger <luojie.5408@163.com>
2018-07-09 10:46:17 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+console.log('Hello, world!')
+
+// const program = require('commander'),
+//   co = require('co');
+
+// const appInfo = require('./../package.json'),
+//   asyncFunc = require('./../common/asyncfunc.js');
+
+// program.allowUnknownOption();
+// program.version(appInfo.version);
+
+// program
+//   .command('init')
+//   .description('初始化当前目录doc.json文件')
+//   .action(() => co(asyncFunc.initAction));
