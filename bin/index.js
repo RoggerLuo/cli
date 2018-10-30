aba5846b5bd55a34a60f2db426f1315dcad3f08e
Roger <luojie.5408@163.com>
2018-10-30 10:29:52 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ var program = require('commander');
 const fs = require('fs')
 
 program.allowUnknownOption();
-program.version('1.0.3');
+program.version('1.0.4');
 
 program
     .command('init')
@@ -20,16 +20,19 @@ program
         }
         console.log('正在从github下载数据')
         shell.exec('git clone https://github.com/RoggerLuo/react-scaffold.git')
+        console.log('ok')
         shell.rm('-rf','react-scaffold/.git')
         shell.exec('mv react-scaffold react-app')
         // shell.exec('cp -rf react-scaffold/* ./')
         // shell.rm('-rf','react-scaffold')
         // console.log('建构完成，请手动执行npm install')
-        shell.exec('cd react-app')
-        shell.exec('npm install')
+        // shell.exec('cd react-app')
+        shell.cd('./react-app');
         console.log('安装依赖..')
-        shell.exec('npm run start')
+        shell.exec('npm install')
+        console.log('ok')
         console.log('启动开发环境..')
+        shell.exec('npm run start')
         shell.exit(1);
     });
 
